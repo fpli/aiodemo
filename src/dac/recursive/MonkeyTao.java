a455bad25a8aebd272ad59bda731037ae898c753
fpli <1394220859@qq.com>
2020-06-19 14:09:11 +0800

    / 2 用 >>1 代替

--- src/dac/recursive/MonkeyTao.java
+++ src/dac/recursive/MonkeyTao.java
@@ -5,63 +5,67 @@ package dac.recursive;
  * 迭代方式实现和递归方式实现
  * 题目：
  * 猴子第一天摘了若干个桃子，当即吃了一半，还不解馋，又多吃了一个； 第二天，吃剩下的桃子的一半，还不过瘾，又多吃了一个；
- * 以后每天都吃前一天剩下的一半多一个，到第10天想再吃时，只剩下一个桃子了。问第一天共吃了多少个桃子？
+ * 以后每天都吃前一天剩下的一半多一个，到第10天想再吃时，只剩下一个桃子了。问第一天摘了多少个桃子？
  * 思路：我们可以采用逆向思维，第十天剩一个，前一天则为 S9 = （S10 +1）*2，以此推算前一天。可以采用递归（下面介绍三种方法）
  * f(n) = f(n-1) / 2 -1;
  * f(n-1) = (f(n) + 1) * 2;
  * f(n) = (f(n+1) + 1) * 2;
- * f(n-1) / 2 - 1 == f(n) == (f(n+1) + 1) * 2
+ *
  */
 public class MonkeyTao {
 
     public static void main(String[] args) {
-        //int amount = total(1);
-        //System.out.println(amount);
+        System.out.println("递归方式实现:");
+        int amount = total(1);
+        System.out.println(amount);
+        System.out.println("迭代方式实现:");
         System.out.println(whileWay());
         System.out.println(forWay());
     }
 
     //解法二 递归实现
     // f(n) = (f(n+1) + 1) * 2
+
     /**
      * day 在[10, 1]区间
+     *
      * @param day
      * @return day 当天的数量
      */
-    public static int total(int day){
-        if (day == 10){
+    public static int total(int day) {
+        if (day == 10) {
             return 1;
         } else {
             return (total(day + 1) + 1) * 2;
         }
     }
 
-    //解法三 while 循环
-    public static int whileWay(){
+    //解法三 while 循环 while循环多用于未知迭代次数，
+    public static int whileWay() {
         int count = 1;   // 第10天 剩1个
         int day = 9;
-        while(day > 0){
-            count = (1+count) * 2;   // next 表示后一天
+        while (day > 0) {
+            count = (1 + count) * 2;   // next 表示后一天
             day--;
         }
         return count;
     }
 
-    //解法一、for 循环实现
-    public static int forWay(){
+    //解法一、for 循环实现 for循环多用于已知迭代次数
+    public static int forWay() {
         int count = 1;            // 第10天 剩1个
-        for(int day = 9; day > 0; day--){
-            count = (1+count) * 2;   // next 表示后一天
+        for (int day = 9; day > 0; day--) {
+            count = (1 + count) * 2;   // next 表示后一天
         }
         return count;
     }
 
-    public static void arr(){
+    public static void arr() {
         int[] array = new int[10];
         array[9] = 1;
         int day = 8;
-        while (day>=0){
-            array[day] = (array[day+1] + 1) * 2;
+        while (day >= 0) {
+            array[day] = (array[day + 1] + 1) * 2;
             day--;
         }
         System.out.println(array[0]);
