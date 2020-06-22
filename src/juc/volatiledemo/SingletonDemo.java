608322ee9061c4a658b6bc8f7fd0650a685caf9f
fpli <1394220859@qq.com>
2020-06-22 15:04:40 +0800

    堆排序是树存储结构的一种应用

--- src/juc/volatiledemo/SingletonDemo.java
+++ src/juc/volatiledemo/SingletonDemo.java
@@ -2,29 +2,29 @@ package juc.volatiledemo;
 
 public class SingletonDemo {
     // DCL机制不一定线程安全，原因是有指令重排的存在，加入volatile可以禁止指令重排
-    private static volatile SingletonDemo singleton = null;
+    private static volatile SingletonDemo instance = null;
 
     private SingletonDemo(){
         System.out.println(Thread.currentThread().getName()+ "\t构造方法执行");
     }
 
     // DCL(Double Check Lock双端检锁机制)
-    public static SingletonDemo getSingleton() {
-        if (null == singleton){
+    public static SingletonDemo getInstance() {
+        if (null == instance){
             synchronized (SingletonDemo.class){
-                if (null == singleton){
-                    singleton = new SingletonDemo();
+                if (null == instance){
+                    instance = new SingletonDemo();
                     // new SingletonDemo(); 分成3步完成 1 分配对象内存空间 2 初始化对象 3 设置intance指向刚分配的内存地址，此时instance != null
                 }
             }
         }
-        return singleton;
+        return instance;
     }
 
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
             new Thread(()->{
-                SingletonDemo.getSingleton();
+                SingletonDemo.getInstance();
             }, String.valueOf(i)).start();
         }
     }
