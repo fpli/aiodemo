898acfa6ef41aad2b58e937f1bead508a0fa2858
fpli <1394220859@qq.com>
2020-07-03 14:59:38 +0800

    java 中取模与求余， 计算商时舍入方向不同，0 还是负无穷方向

--- src/dac/graph/GraphDemo.java
+++ src/dac/graph/GraphDemo.java
@@ -1,4 +1,16 @@
 package dac.graph;
 
+import java.math.BigInteger;
+
 public class GraphDemo {
+
+    public static void main(String[] args) {
+        System.out.println(Math.floorMod(4, -3));
+        System.out.println(-4 % 3);
+
+        BigInteger b1 = new BigInteger("-23");
+        BigInteger b2 = new BigInteger("3");
+        System.out.println(b1.mod(b2));// BigInteger 取模时，除数不能是负数
+        System.out.println(b1.remainder(b2));
+    }
 }
--- src/lock/MyThreadPoolExecutor.java
+++ src/lock/MyThreadPoolExecutor.java
@@ -3,11 +3,12 @@ package lock;
 import java.util.concurrent.*;
 
 public class MyThreadPoolExecutor {
+
     public static void main(String[] args) {
         ExecutorService executorService = new ThreadPoolExecutor(0, 5, 1, TimeUnit.SECONDS, new LinkedBlockingQueue<>(3), Executors.defaultThreadFactory(), new ThreadPoolExecutor.CallerRunsPolicy());
-        for (int i = 0; i < 20; i++) {
-            executorService.execute(()->{
-                System.out.println(Thread.currentThread().getName() + "\t执行业务");
+        for (int i = 0; i < 200; i++) {
+            executorService.execute(() -> {
+                System.out.println(Thread.currentThread().getName() + "\t执行业务" + (1 << 13));
             });
         }
     }
