19d4d0c8dc9220d427d83826e99ed2dafd248bfc
fpli <1394220859@qq.com>
2020-12-24 16:33:31 +0800

    socketChannel

--- src/nio/proxy/Client.java
+++ src/nio/proxy/Client.java
@@ -5,7 +5,7 @@ public class Client {
     public static void main(String[] args) {
         NetClient netClient = new NetClient();
         for (int i = 0; i < 10; i++) {
-            UserServiceI userServiceI = (UserServiceI) netClient.getBean(UserServiceI.class);
+            UserServiceI userServiceI = netClient.getBean(UserServiceI.class);
             System.out.println(userServiceI.getClass());
             System.out.println(userServiceI.say("invoke target by proxy"));
         }
--- src/nio/proxy/NetClient.java
+++ src/nio/proxy/NetClient.java
@@ -1,14 +1,15 @@
 package nio.proxy;
 
 import java.lang.reflect.Proxy;
+import java.util.Arrays;
 
 public class NetClient {
 
-    private UserServiceI userServiceI = new UserServiceImpl();
+    private final UserServiceI userServiceI = new UserServiceImpl();
 
-    public Object getBean(Class<?> serviceClass){
-       return Proxy.newProxyInstance(Thread.currentThread().getContextClassLoader(), new Class[]{serviceClass}, (proxy, method, args) -> {
-           System.out.println(proxy.getClass());
+    public <T> T getBean(Class<T> serviceClass){
+       return (T)Proxy.newProxyInstance(Thread.currentThread().getContextClassLoader(), new Class[]{serviceClass}, (proxy, method, args) -> {
+           System.out.println(proxy.getClass() +":"+ Arrays.toString(args));
            return userServiceI.say((String) args[0]);
        });
     }
